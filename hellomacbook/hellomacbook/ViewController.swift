947ff6601d013a220d649bdb8f842f67da371119
DoCE <briancho325@gmail.com>
2022-04-05 02:55:53 +0900

    0405-1

---
+++ hellomacbook/hellomacbook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hellomacbook
+//
+//  Created by 조현석 on 2022/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+// hello this is a mac git test
